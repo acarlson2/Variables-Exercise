663dddadc62fe0c55742d832651349634f615d86
Aaron Carlson <acarlson2@carthage.edu>
2024-05-31 10:58:39 -0500

    exercise complete

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Jameson";
+            int age = 45;
+            char favLetter = 'e';
+            bool isMarried = false;
+            double moneyOnHand = 50.25;
+            decimal favNumber = 260.123M;
+
+            Console.WriteLine($"This is the story of {name}, a man who is {age} years old.");
+            Console.WriteLine($"His favorite letter is {favLetter}, and his favorite number is {favNumber}.");
+            Console.WriteLine($"He marrital status is {isMarried} and he currently has ${moneyOnHand} on his person.");
         }
     }
 }
